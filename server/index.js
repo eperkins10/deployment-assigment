252b77dfd55accaa10274b7e08d1a77826172bc9
Ethan Perkins <ethanperkinspro@gmail.com>
2021-10-21 14:13:11 -0600

    added /css get request

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
+app.get("/css", (req, res) => {
+    res.sendFile(path.join(__dirname, "../styles.css"))
+})
+
 const port = process.env.PORT || 4567
 
 app.listen(port, () => console.log(`runnin on ${port} ong`))
